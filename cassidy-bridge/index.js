aea3f3bb1a9cd556974568d1cd9354b9d16c3e30
Liane Cagara (LiANE) <150021684+lianecagara@users.noreply.github.com>
2024-10-09 06:51:39 +0000

    Fixes

--- cassidy-bridge/index.js
+++ cassidy-bridge/index.js
@@ -1,7 +1,11 @@
 const axios = require("axios");
 
 class CassidyBridge {
-  constructor({ url = "https://cassidybot.onrender.com", onGetAllData, onSetEachData }) {
+  constructor({
+    url = "https://cassidybot.onrender.com",
+    onGetAllData,
+    onSetEachData,
+  }) {
     this.onGetAllData = onGetAllData;
     this.onSetEachData = onSetEachData;
     this.url = url;
@@ -10,26 +14,33 @@ class CassidyBridge {
   async sendQuery(event) {
     try {
       const databaseData = await this.onGetAllData();
-      const response = await axios.get(this.url + "/postWReply", {
-        params: { ...event, localDB: true, databaseData },
+      const response = await axios.post(this.url + "/postNew", {
+        ...event,
+        localDB: true,
+        databaseData,
       });
 
       const { status, modifiedData, result } = response.data;
 
       if (modifiedData) {
-        await Promise.all(Object.keys(modifiedData).map(userID =>
-          this.onSetEachData(userID, modifiedData[userID])
-        ));
+        await Promise.all(
+          Object.keys(modifiedData).map((userID) =>
+            this.onSetEachData(userID, modifiedData[userID]),
+          ),
+        );
       }
 
       return status === "success" ? result : null;
     } catch (error) {
       console.error("Error sending query:", error);
-      return null;
+      throw error;
     }
   }
   async goatQuery(message, event) {
     const result = await this.sendQuery(event);
+    if (!result) {
+      return;
+    }
     return await message[result.isReply ? "reply" : "send"](result.body);
   }
 
@@ -43,9 +54,9 @@ class CassidyBridge {
         }, {});
       } catch (error) {
         console.error("Error getting all data:", error);
-        return {};
+        throw error;
       }
-    }
+    };
 
     const onSetEachData = async (userID, data) => {
       try {
@@ -56,8 +67,9 @@ class CassidyBridge {
         });
       } catch (error) {
         console.error(`Error setting data for user ${userID}:`, error);
+        throw error;
       }
-    }
+    };
 
     return new CassidyBridge({ ...etc, onGetAllData, onSetEachData });
   }
